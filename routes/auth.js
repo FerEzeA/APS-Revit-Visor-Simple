c15d5300adc854b3134ea24ea66bd20900edb4c5
Fernando Albornoz <fernando_albornoz044@hotmail.com>
2023-07-04 19:00:33 -0300

    modified

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const { getPublicToken } = require('../services/aps.js');
 let router = express.Router();
 
 router.get('/api/auth/token', async function (req, res, next) {
+
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     try {
         res.json(await getPublicToken());
     } catch (err) {
