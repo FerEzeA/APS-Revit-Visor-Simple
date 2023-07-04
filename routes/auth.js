e457a0029307893eb91d1c1d7ca339691e3d8f99
Fernando Albornoz <fernando_albornoz044@hotmail.com>
2023-07-04 19:16:35 -0300

    modified

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,11 @@ router.get('/api/auth/token', async function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     try {
-        res.json(await getPublicToken());
+        const token = await getPublicToken()
+        res.json({
+            access_token: token.access_token,
+            expires_in: token.expires_in    
+        });
     } catch (err) {
         next(err);
     }
